37bdb3d88fcc961a85f5feb783b60188780bad8e
Keagan Eng <grace.eng02@gmail.com>
2022-01-23 12:22:31 -0600

    Progress bar movement on page changes

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-var numQuestions = 5;
+var numQuestions = 4;
 var currentPoints = 0;
 var barPoints = 10;
-var pointsPerQuestion = 10/numQuestions;
-if (moreAtRiskOption) {
-	currentPoints += pointsPerQuestion;
+var pointsPerQuestion = 9.5/numQuestions;
+function getPoints(){
+    currentPoints = JSON.parse(localStorage.getItem('currentPoints')); //getter
+    document.getElementById('myTriangle').style.marginLeft = currentPoints*10 + "%";
+    console.log(currentPoints);
 }
-else {
-    currentPoints -= pointsPerQuestion;
+
+function updatePoints(){
+    if (document.getElementById("Q1").value > 7) {
+        currentPoints += pointsPerQuestion;
+        currentPoints += pointsPerQuestion;
+    } else if (document.getElementById("Q1").value > 3) {
+        currentPoints += pointsPerQuestion;
+    } else if (document.getElementById("Q2B").checked) {
+        currentPoints += pointsPerQuestion;
+        currentPoints += pointsPerQuestion;
+    } else if (document.getElementById("Q2A").checked) {
+        currentPoints += pointsPerQuestion;
+    } 
+    localStorage.setItem("currentPoints", JSON.stringify(currentPoints)); //setter
+    console.log(currentPoints);
 }
-document.getElementById('myTriangle').style.width = currentPoints * 100 + "%";
+
+// document.getElementById('myTriangle').style.marginLeft = "75%";
